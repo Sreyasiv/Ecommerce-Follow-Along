7f7f6b6dc9f27f9b67a13267764a833eb1ea3111
Sreya Sivakumar <sreyas@Sreyas-MacBook-Air.local>
2025-01-27 17:40:25 +0530

    m4_updated

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,23 @@
 const express=require("express");
 const app=express();
 const ErrorHandler=require("./middleware/error");
+const cookieParser=require("cookie-parser");
+const bodyParser=require("body-parser");
 
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"));
+app.use(bodyParser.urlencoded({extended:true,limit:"50mb"}));
 //config
 if(process.env.NODE_ENV !== "PRODUCTION"){
     require("dotenv").config({
         path:"backend/config/.env",
 });
 };
+//import routes
+const user=require("./controller/user.js");
+app.use("/api/v2/user",user);
 
-//app.use(ErrrorHandler);
+app.use(ErrorHandler);
 module.exports=app;
